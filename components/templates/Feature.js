bead5c2837cb13e1ce965f951130e4c1b010bb76
Reza Hamzei <hamzeiamir205@gmail.comls>
2023-11-30 11:24:57 +0330

    editing feature.js styles

--- components/templates/Feature.js
+++ components/templates/Feature.js
@@ -3,7 +3,7 @@ import React from "react";
 const Feature = () => {
   return (
     <div>
-      <h3 className="font-bold mb-4">برترین خانه های لوکس جهان</h3>
+      <h3 className="font-bold mb-4 text-xl">برترین خانه های لوکس جهان</h3>
       <p>
         همچنین طراح به دنبال این است که پس از ارایه کار نظر دیگران را در مورد
         طراحی جویا شود و نمی‌خواهد افراد روی متن های موجود تمرکز کنند.
